c9523572e680a7f0ef2a6bc574ca83d27635cac2
Rob Lihou <rob@visiosound.co.uk>
2018-03-19 16:23:37 +0000

    Used before each in port.test to create an instance of the port object to be used in all the tests

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -5,34 +5,43 @@ const { Port } = require('../src/Port.js');
 const { Itinerary } = require('../src/Itinerary.js');
 
 describe('Port', () => {
+
   it('can be instantiated', () => {
     expect(new Port()).toBeInstanceOf(Object);
   });
 
-  it('port has name', () => {
-    const port = new Port('Dover');
-    expect(port.portName).toBe('Dover');
-  });
+  describe('with a port', () => {
+    let port;
 
-  it('port has ships', () => {
-    const port = new Port('Dover');
-    expect(port.ships).toEqual([]);
-  });
+    beforeEach(() => {
+      port = new Port('Dover');
+    });
 
-  it('port can add ships', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const robShip = new Ship(itinerary);
-    port.addShip(robShip);
-    expect(port.ships).toContain(robShip);
-  });
+    it('port has name', () => {
+      // const port = new Port('Dover');
+      expect(port.portName).toBe('Dover');
+    });
 
-  it('port can remove ships', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const robShip = new Ship(itinerary);
-    const anthonyShip = new Ship(itinerary);
-    port.removeShip(robShip);
-    expect(port.ships).toEqual([anthonyShip]);
+    it('port has ships', () => {
+      // const port = new Port('Dover');
+      expect(port.ships).toEqual([]);
+    });
+
+    it('port can add ships', () => {
+      // const port = new Port('Dover');
+      const itinerary = new Itinerary([port]);
+      const robShip = new Ship(itinerary);
+      port.addShip(robShip);
+      expect(port.ships).toContain(robShip);
+    });
+
+    it('port can remove ships', () => {
+      // const port = new Port('Dover');
+      const itinerary = new Itinerary([port]);
+      const robShip = new Ship(itinerary);
+      const anthonyShip = new Ship(itinerary);
+      port.removeShip(robShip);
+      expect(port.ships).toEqual([anthonyShip]);
+    });
   });
 });
