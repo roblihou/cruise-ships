8554c40e05e7b9b7b47b9da2006c3c205b0679bb
Rob Lihou <rob@visiosound.co.uk>
2018-03-19 16:03:14 +0000

    Used before each to dry up the ship test

--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -4,60 +4,72 @@ const { Ship } = require('../src/Ship.js');
 const { Port } = require('../src/Port.js');
 const { Itinerary } = require('../src/Itinerary.js');
 
+
 describe('Ship', () => {
-  it('can be instantiated', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    expect(ship).toBeInstanceOf(Object);
-  });
+  describe('with a port and itinerery', () => {
+    let ship;
+    let port;
 
-  it('has a starting port', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    expect(ship.currentPort).toBe(port);
-  });
+    beforeEach(() => {
+      port = new Port('Dover');
+      const itinerary = new Itinerary([port]);
+      ship = new Ship(itinerary);
+    });
 
-  it('has set sail', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    ship.setSail();
-    expect(ship.currentPort).toBeFalsy();
-  });
+    it('can be instantiated', () => {
+      //  const port = new Port('Dover');
+      //  const itinerary = new Itinerary([port]);
+      //  const ship = new Ship(itinerary);
+      expect(ship).toBeInstanceOf(Object);
+    });
 
-  it('is docked at a port', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    const calais = new Port('Calais');
-    ship.dock(calais);
-    expect(ship.currentPort).toBe(calais);
-  });
+    it('has a starting port', () => {
+      //  const port = new Port('Dover');
+      // const itinerary = new Itinerary([port]);
+      // const ship = new Ship(itinerary);
+      expect(ship.currentPort).toBe(port);
+    });
 
-  it('gets added to port on instantiation', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    expect(ship.currentPort.ships).toContain(ship);
-  });
+    it('has set sail', () => {
+      //  const port = new Port('Dover');
+      // const itinerary = new Itinerary([port]);
+      // const ship = new Ship(itinerary);
+      ship.setSail();
+      expect(ship.currentPort).toBeFalsy();
+    });
 
-  it('gets removed from port when it sets sail', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    ship.setSail();
-    expect(port.ships).not.toContain(ship);
-  });
+    it('is docked at a port', () => {
+      //  const port = new Port('Dover');
+      // const itinerary = new Itinerary([port]);
+      // const ship = new Ship(itinerary);
+      const calais = new Port('Calais');
+      ship.dock(calais);
+      expect(ship.currentPort).toBe(calais);
+    });
 
-  it('can dock at a different port', () => {
-    const port = new Port('Dover');
-    const itinerary = new Itinerary([port]);
-    const ship = new Ship(itinerary);
-    ship.setSail();
-    const nextPort = new Port('Ningbo'); 
-    ship.dock(nextPort);
-    expect(nextPort.ships).toContain(ship);
+    it('gets added to port on instantiation', () => {
+      //  const port = new Port('Dover');
+      // const itinerary = new Itinerary([port]);
+      // const ship = new Ship(itinerary);
+      expect(ship.currentPort.ships).toContain(ship);
+    });
+
+    it('gets removed from port when it sets sail', () => {
+      //  const port = new Port('Dover');
+      // const itinerary = new Itinerary([port]);
+      // const ship = new Ship(itinerary);
+      ship.setSail();
+      expect(port.ships).not.toContain(ship);
+    });
+
+    it('can dock at a different port', () => {
+      //  const port = new Port('Dover');
+      // const itinerary = new Itinerary([port]);
+      // const ship = new Ship(itinerary);
+      ship.setSail();
+      const nextPort = new Port('Ningbo');
+      ship.dock(nextPort);
+      expect(nextPort.ships).toContain(ship);
+    });
   });
 });
